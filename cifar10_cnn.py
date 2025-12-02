b0a50ff1922c931a948ce8d1afedd2f2669f0c4a
Arthur Jenck <arthurjenckdev@gmail.com>
2025-12-02 14:46:36 +0100

    feat/cnn: added sequential structure and summary

--- cifar10_cnn.py
+++ cifar10_cnn.py
@@ -20,3 +20,21 @@ plt.show()
 
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
+model = models.Sequential([
+    layers.Input(shape=(32,32,3)),
+    layers.Conv2D(32, (3,3), activation='relu'),
+    layers.MaxPooling2D((2,2)),
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D((2,2)),
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+model.summary()
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
